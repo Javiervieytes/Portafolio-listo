8b869066d11712da7c3b3f63ed3650742ca91ca6
Javiervieytes <163855235+Javiervieytes@users.noreply.github.com>
2025-10-11 11:59:30 -0300

    Add files via upload

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/organisms/NavBar";
+
+function NotFound() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="main-bg">
+      <NavBar />
+
+      <div className="notfound-container">
+        <div className="notfound-box">
+          <h1>404</h1>
+        </div>
+
+        <div className="notfound-box">
+          <p>Página no encontrada</p>
+        </div>
+
+        <button
+          className="notfound-button"
+          onClick={() => navigate(-1)}
+        >
+          Volver al inicio
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
